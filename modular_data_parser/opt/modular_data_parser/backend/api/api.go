77987359f8d7b2c1530b7391bc5e09621074994e
wjdavie <wjdavie@localhost>
2022-02-01 21:31:33 +0000

    Adjusted api to variable for easier starting

--- modular_data_parser/opt/modular_data_parser/backend/api/api.go
+++ modular_data_parser/opt/modular_data_parser/backend/api/api.go
@@ -2,23 +2,25 @@ package api
 
 import (
 	"fmt"
-	"net/http"
 	"log"
+	"net/http"
 )
 
-// Home page 
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+// Home page
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
 }
+
 // Request handler
-func handleRequests() {
-    http.HandleFunc("/", homePage)
-    log.Fatal(http.ListenAndServe(":3000", nil))
+func handleRequests(api string) {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", api), nil))
 }
 
 // API loop for requests is here:
-func MainApiLoop(){
+func MainApiLoop() {
 	fmt.Println("Starting API....")
-	handleRequests()
+	apiPort := "3000"
+	handleRequests(api)
 }
