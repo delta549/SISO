e7c6ab69f078e41c9b92ff1a2c5f6862d88e8a5d
delta549 <wez5444@gmail.com>
2022-02-04 12:13:52 +0000

    Added icon from Icons-8

--- modular_data_parser/opt/modular_data_parser/frontend/src/components/MenuAppBar.js
+++ modular_data_parser/opt/modular_data_parser/frontend/src/components/MenuAppBar.js
@@ -3,28 +3,21 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+//import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import Icons8 from './icons8-ship-64.png'
 
 export default function MuttonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
+          <IconButton>
+            <img  src={Icons8} alt=""/>
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            ShipInShipOut
           </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
