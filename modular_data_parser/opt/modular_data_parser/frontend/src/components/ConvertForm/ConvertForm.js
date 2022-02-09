669fa1aa89fa3e61432678dbe6e894cc7847d170
wjdavie <wjdavie@localhost>
2022-02-09 00:22:43 +0000

    It's a start....

--- modular_data_parser/opt/modular_data_parser/frontend/src/components/ConvertForm/ConvertForm.js
+++ modular_data_parser/opt/modular_data_parser/frontend/src/components/ConvertForm/ConvertForm.js
@@ -1,118 +1,56 @@
 import './ConvertForm.css';
+import { Component } from 'react';
 
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { typography } from '@mui/system';
+class ConvertForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      valueOne: 'JSON',
+      valueTwo: 'JSON'
+    };
+    this.handleChangeFirst = this.handleChangeFirst.bind(this);
+    this.handleChangeSecond = this.handleChangeSecond.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-const steps = ['Choose Data upload format', 'Choose what to convert too', 'Download converted data'];
+  handleChangeFirst(event) {
+    this.setState({valueOne: event.target.value});
+    console.log(this.state.valueOne)
+  }
 
-export default function HorizontalLinearStepper() {
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [skipped, setSkipped] = React.useState(new Set());
+  handleChangeSecond(event) {
+    this.setState({valueTwo: event.target.value});
+  }
 
-  const isStepOptional = (step) => {
-    return step === 1;
-  };
+  handleSubmit(event) {
+    alert('Your first selection is: ' + this.state.valueOne + ' Your second is: ' + this.state.valueTwo);
 
-  const isStepSkipped = (step) => {
-    return skipped.has(step);
-  };
+    event.preventDefault();
+  }
 
-  const handleNext = () => {
-    let newSkipped = skipped;
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values());
-      newSkipped.delete(activeStep);
-    }
-
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped(newSkipped);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleSkip = () => {
-    if (!isStepOptional(activeStep)) {
-      // You probably want to guard against something like this,
-      // it should never occur unless someone's actively trying to break something.
-      throw new Error("You can't skip a step that isn't optional.");
-    }
-
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped((prevSkipped) => {
-      const newSkipped = new Set(prevSkipped.values());
-      newSkipped.add(activeStep);
-      return newSkipped;
-    });
-  };
-
-  const handleReset = () => {
-    setActiveStep(0);
-  };
-
-  return (
-    <Box sx={{ width: '100%' }}>
-      <Stepper activeStep={activeStep}>
-        {steps.map((label, index) => {
-          const stepProps = {};
-          const labelProps = {};
-          if (isStepOptional(index)) {
-            labelProps.optional = (
-              <Typography variant="caption">Optional</Typography>
-            );
-          }
-          if (isStepSkipped(index)) {
-            stepProps.completed = false;
-          }
-          return (
-            <Step key={label} {...stepProps}>
-              <StepLabel {...labelProps}>{label}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
-      {activeStep === steps.length ? (
-        <React.Fragment>
-          <Typography variant='white' sx={{ mt: 2, mb: 1 }}>
-            All steps completed - you&apos;re finished
-          </Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Box sx={{ flex: '1 1 auto' }} />
-            <Button onClick={handleReset}>Reset</Button>
-          </Box>
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          <Typography variant='h4' sx={{ mt: 2, mb: 1 }} color="white" >Step {activeStep + 1}</Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
-            >
-            <Typography variant='white'>Back</Typography>
-            </Button>
-            <Box sx={{ flex: '1 1 auto' }} />
-            {isStepOptional(activeStep) && (
-              <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
-                Skip
-              </Button>
-            )}
-
-            <Button onClick={handleNext}>
-              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-            </Button>
-          </Box>
-        </React.Fragment>
-      )}
-    </Box>
-  );
+  render() {
+    return (
+      <div className='ConvertForm'>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <select value={this.state.valueOne} onChange={this.handleChangeFirst}>
+              <option value="CSV">CSV</option>
+              <option value="JSON">JSON</option>
+              <option value="TSV">TSV</option>
+            </select>
+          </label>
+          <label>
+            <select value={this.state.valueTwo} onChange={this.handleChangeSecond}>
+              <option value="CSV">CSV</option>
+              <option value="JSON">JSON</option>
+              <option value="TSV">TSV</option>
+            </select>
+          </label>
+          <input type="submit" value="Convert" />
+        </form>
+      </div>
+    );
+  }
 }
+
+export default ConvertForm
